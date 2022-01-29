4ef1f42335c87554b11b1e15be48cfb5b3d6a9d3
Andrew Strelcov <astrelcov@yahoo.com>
2022-01-29 23:21:38 +0200

    add html base template

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,2 +1,200 @@
-<?php 
-echo "dsjgjdgj";
+<!DOCTYPE html>
+<html class="page" lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Document</title>
+</head>
+
+<body class="page-body">
+    <header class="main-header">
+        <nav class="top-bar">
+            <div class="top-bar__wrap container">
+                <div class="top-bar__city">г.Алчевск ул.Бандеры 13</div>
+                <ul class="top-bar__list">
+                    <li class="top-bar__phone1">
+                        <a class="top-bar__link" href="tel:+380508888888">(050)888-88-88</a>
+                    </li>
+                    <li class="top-bar__phone2">
+                        <a class="top-bar__link" href="tel:+380677777777">(067)777-77-77</a>
+                    </li>
+                </ul>
+                <div class="time-works">
+                    <p>График работы: ПН-ПТ с
+                        <span class="time-works_blue"> 9-00</span>
+                        до
+                        <span class="time-works_blue"> 18-00</span>,
+                        СБ с
+                        <span class="time-works_blue">10-00</span>
+                        до
+                        <span class="time-works_blue">16-00</span>
+                    </p>
+                </div>
+            </div>
+        </nav>
+        <nav class="main-menu">
+            <div class="container main-menu__wrap">
+                <div class="header-logo">
+                    <a href="/" class="header-logo__link">
+                        <img src="images/dest/logo.svg" alt="" class="">
+                    </a>
+                </div>
+                <div class="header-burger">
+                    <span></span>
+                </div>
+                <ul class="main-menu-list">
+                    <li class="main-menu__item">
+                        <a class="main-menu__link" href="windows.html">Окна</a>
+                    </li>
+                    <li class="main-menu__item">
+                        <a class="main-menu__link" href="/">Двери</a>
+                    </li>
+                    <li class="main-menu__item">
+                        <a class="main-menu__link" href="/">Балконы</a>
+                    </li>
+                    <li class="main-menu__item">
+                        <a class="main-menu__link" href="/">Жалюзи</a>
+                    </li>
+                    <li class="main-menu__item">
+                        <a class="main-menu__link" href="/">Контакты</a>
+                    </li>
+                </ul>
+                <div class="btn menu-button">
+                    <a href="" class="menu-button__link">Заказать звонок</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main class="main-page">
+        <section class="hero">
+            <div class="hero-content container">
+                <div class="hero-action">
+                    <p class="hero-title">Специальное предложение</p>
+                    <p class="hero-subtitle">Пластиковые окна со скидкой</p>
+                </div> 
+                <div class="hero-form">
+                    <p class="hero-form__text">Оставьте заявку на бесплатную 
+                                консультацию менеджера
+                    </p>
+                    <div class="form-wrap">
+                        <form class="form" action="">
+                            <input id="phone" class="form__input" type="phone" placeholder="+38(050)888-88-88">
+                        </form>
+                        <button type="submit" class="btn hero-btn">Заказать звонок</button>
+                    </div>
+            </div>
+        </section>
+        <section class="features">
+            <ul class="features-list container">
+                <li class="features-item_blue">
+                    <h3 class="features-title_blue">Обратитесь в надежную компанию</h3>
+                    <p class="features-content_blue">Более 15 лет 
+                        профессионального опыта. 
+                        Окна, двери и аксессуары
+                        к ним соответствуют всем 
+                        необходимым нормам 
+                        экологичности, безопасности 
+                        и энергосбережения.</p>
+                </li>
+                <li class="features-item">
+                    <div class="features-icon">
+                        <img src="/images/dest/ic_designer_card 1.svg" alt="">
+                    </div>
+                    <h3 class="">Гарантия<br>
+                        качества 10 лет</h3>
+                    <p class="features-content">Окна от Альянс Строй – это 
+                        качественный профиль, 
+                        энергосберегающие 
+                        стеклопакеты и европейская 
+                        фурнитура. Гарантии 
+                        оправданы качеством!</p>
+                </li>
+                <li class="features-item">
+                    <div class="features-icon">
+                        <img src="/images/dest/ic_designer_card 1.svg" alt="">
+                    </div>
+                    <h3 class="">Профессиональный 
+                        монтаж</h3>
+                    <p class="features-content">Качественный монтаж – 
+                        залог качественной работы 
+                        оконной системы в течение 
+                        всего срока эксплуатации. 
+                        В Альянс Строй – 
+                        профессионалы монтажа!</p>
+                </li>
+                <li class="features-item">
+                    <div class="features-icon">
+                        <img src="/images/dest/ic_designer_card 1.svg" alt="">
+                    </div>
+                    <h3 class="">Бесплатный<br>
+                        замер</h3>
+                    <p class="features-content">Установка окон начинается 
+                        с замеров оконных проемов. 
+                        В Альянс Строй 
+                        профессиональные замеры 
+                        оконных и дверных 
+                        проемов осуществляются 
+                        бесплатно!</p>
+                </li>
+            </ul>
+        </section>
+        <section class="windows">
+            <div class="win-wrap container">
+                <div class="">
+                    <p class="">Пластиковые окна</p>
+                    <h2 class="">Работаем с 2003 года. 
+                                Знаем о окнах все!</h2>
+                    <ul class="windows-list">
+                        <li class="windows-item">Нестандартная форма</li>
+                        <li class="windows-item">Противовзломная фурнитура</li>
+                        <li class="windows-item">Энергосберегающие стеклопакеты</li>
+                        <li class="windows-item">Москитные сетки</li>
+                        <li class="windows-item">Подоконники и отливы</li>
+                    </ul>
+                    <a class="btn" href="">Подробнее</a>
+                </div>
+                <div class="img-block">
+                        <img src="/images/dest/win-main.jpg" alt="" class="">
+                </div>
+            </div>
+        </section>
+        <section class="doors">
+            <h2 class="test">Входные двери</h2>
+            <p class=""></p>
+        </section>
+        <section class="dlind">
+
+        </section>
+        <section class="zamer">
+
+        </section>
+        <section class="otziv">
+
+        </section>
+        <section class="">
+            <h2>Наши контакты</h2>
+            <div>
+                <ul>
+                    <li class=""></li>
+                    <li class=""></li>
+                    <li class=""></li>
+                </ul>
+            </div>
+            <figure>
+
+            </figure>
+        </section>
+    </main>
+    <footer class="">
+
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="js/app.min.js"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use App\Http\Controllers\Admin\MainController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function ()
@@ -24,7 +24,9 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function ()
     Route::get('/', [MainController::class, "index"])->name('admin.index');
 });
 
-
+Route::get('/', function () {
+    return view('base');
+});
 
 
 Route::get('/windows', function () {
